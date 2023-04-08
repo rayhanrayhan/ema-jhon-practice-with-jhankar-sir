cc687a950de38b0d6f185907f5fb77f854633edb
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-04-09 00:28:29 +0600

    first commit

---
+++ src/loaders/cartProductsLoader.js
@@ -0,0 +1,24 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+const cartProductsLoader = async ()  => {
+const loadedProducts = await fetch ('products.json')
+const products = await loadedProducts.json();
+
+
+// if card data is in database , you have to ues async await 
+
+const storedCart = getShoppingCart();
+const savedCart = [];
+ for (const id in storedCart){
+    const addedProduct = products.find(pd => pd.id === id);
+    if(addedProduct){
+        const quantity = storedCart[id];
+        addedProduct.quantity=  quantity;
+        savedCart.push(addedProduct);
+
+    }
+ }
+return savedCart;
+}
+
+export  default cartProductsLoader;
